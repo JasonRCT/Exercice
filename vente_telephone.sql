7640f6d2a981e7e46ac595c92a59544c24cce5bf
Rouchette <jason.rouchette@gmail.com>
2023-10-20 16:37:23 +0200

    modification du values

--- vente_telephone.sql
+++ vente_telephone.sql
@@ -4,17 +4,17 @@ USE vente_telephone;
 
 CREATE TABLE telephone (
     id INT UNSIGNED PRIMARY KEY auto_increment,
-    marque VARCHAR(70) NOT NULL,
+    marque VARCHAR(70), NOT NULL,
     modele VARCHAR(70),
     capacite VARCHAR(5),
     couleur VARCHAR(70)
 );
 
-INSERT INTO TABLE telephone ("marque", "modele", "capacite", "couleur")
+INSERT INTO TABLE telephone ('marque', 'modele', 'capacite', 'couleur')
 VALUES
-("iphone", "15", "128go", "rouge"),
-("iphone", "13", "64go", "bleu"),
-("samsung", "s23", "128go", "blanc");
+('iphone', '15', '128go', 'rouge'),
+('iphone', '13', '64go', 'bleu'),
+('samsung', 's23', '128go', 'blanc');
 
 SHOW DATABASES;
 
@@ -22,3 +22,6 @@ SHOW TABLES;
 
 DESCRIBE telephone;
 
+DROP TABLE IF EXISTS vente_telephone;
+
+DROP DATABASE IF EXISTS vente_telephone;
