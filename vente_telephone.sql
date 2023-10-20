b63120f704782a21dacf324a1577d33514708cb1
Rouchette <jason.rouchette@gmail.com>
2023-10-20 16:28:22 +0200

    ajout values sur sql

--- vente_telephone.sql
+++ vente_telephone.sql
@@ -2,22 +2,23 @@ Create database IF NOT EXISTS vente_telephone CHARACTER set "utf8" COLLATE = "ut
 
 USE vente_telephone;
 
-CREATE TABLE clients (
+CREATE TABLE telephone (
     id INT UNSIGNED PRIMARY KEY auto_increment,
     marque VARCHAR(70) NOT NULL,
     modele VARCHAR(70),
-    capacité VARCHAR(5),
-    couleur VARCHAR(70),
-    gravure TEXT
+    capacite VARCHAR(5),
+    couleur VARCHAR(70)
 );
+
+INSERT INTO TABLE telephone ("marque", "modele", "capacite", "couleur")
+VALUES
+("iphone", "15", "128go", "rouge"),
+("iphone", "13", "64go", "bleu"),
+("samsung", "s23", "128go", "blanc");
 
 SHOW DATABASES;
 
 SHOW TABLES;
 
-DESCRIBE clients;
-
-DROP TABLE IF EXISTS vente_telephone;
-
-DROP DATABASE IF EXISTS vente_telephone;
+DESCRIBE telephone;
 
